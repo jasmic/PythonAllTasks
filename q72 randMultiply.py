acb60722e04fdbf0f28c0237255bab007d0fcc73
MrHawkins-Babington <95847296+MrHawkins-Babington@users.noreply.github.com>
2021-12-26 10:04:55 +0000

    Add files via upload

---
+++ q72 randMultiply.py
@@ -0,0 +1,13 @@
+#PythonAllTasks q72 randMultiply
+
+import random
+
+for i in range(5):
+    first = random.randint(1,10)
+    second = random.randint(1,10)
+    answer = first*second
+    guess = int(input(str(first)+"X"+str(second)+"="))
+    if guess == answer:
+        print("Correct answer")
+    else:
+        print("Incorrect answer")
