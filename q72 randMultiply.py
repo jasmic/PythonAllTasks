5d2f4912c51f2d600e4b87f2e651fd61360d3ceb
MrHawkins-Babington <95847296+MrHawkins-Babington@users.noreply.github.com>
2021-12-26 10:08:31 +0000

    Update q72 randMultiply.py

--- q72 randMultiply.py
+++ q72 randMultiply.py
@@ -6,7 +6,7 @@ for i in range(5):
     first = random.randint(1,10)
     second = random.randint(1,10)
     answer = first*second
-    guess = int(input(str(first)+"X"+str(second)+"="))
+    guess = int(input(str(first)+" X "+str(second)+" = "))
     if guess == answer:
         print("Correct answer")
     else:
