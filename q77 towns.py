2f3229d79e31ef8a51a5d670dc2675fad033b3fd
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-01 10:52:12 +0000

    Add files via upload

---
+++ q77 towns.py
@@ -0,0 +1,11 @@
+#PythonAllTasks q76 towns
+
+array = ["Barton","Uttoxeter","Walsall","Loxley"]
+print(array[0:1])
+print(array[3:4])
+print(array[0:2])
+
+
+
+
+#print(array[1:3]) #prints Uttoxeter Loxley
