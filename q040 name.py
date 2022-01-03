0454c3fd67f2df53ceed3417a4cfe8f1a5eeb655
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-03 17:51:25 +0000

    Add files via upload

---
+++ q040 name.py
@@ -0,0 +1,11 @@
+#Python all tasks q40
+
+firstname = input("Please enter your first name")
+surname = input("Please enter your surname")
+
+print(firstname[0:1])
+print(firstname[0:4])
+print(surname[-1])
+
+
+
