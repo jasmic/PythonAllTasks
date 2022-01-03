72317b6814fa2775a52fc730551f212a48caca1e
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-03 14:16:57 +0000

    Add files via upload

---
+++ q038 nameLen.py
@@ -0,0 +1,9 @@
+#Python all tasks q38
+
+firstName = input("what is your first name?")
+surName = input("What is your surname?")
+fullName = firstName+surName
+print(len(fullName))
+
+
+
---
+++ q039 colour.py
@@ -0,0 +1,11 @@
+#Python all tasks q39
+
+colour = input("Please enter a colour that has 6 letters in its name")
+colourLength = len(colour)
+if colourLength == 6:
+    print("Well done!")
+else:
+    print("Incorrect")
+
+
+
