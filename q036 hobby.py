b9992a38d881727a4951816f1f09935269dd41b0
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-03 14:07:01 +0000

    Add files via upload

---
+++ q036 hobby.py
@@ -0,0 +1,6 @@
+#Python all tasks q36
+
+hobby1 = input("User1, what is your hobby?")
+hobby2 = input("User1's hobby is "+hobby1+", what is yours?")
+print("User1's hobby is",hobby1,"and User2's hobby is",hobby2)
+
---
+++ q037 drink.py
@@ -0,0 +1,8 @@
+#Python all tasks q37
+
+drink = input("what is your favourite drink?")
+drinkLength = len(drink)
+print(drinkLength)
+
+
+
