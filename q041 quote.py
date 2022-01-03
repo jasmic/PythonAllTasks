096b2d994add3585d5396bd94f3799bcfec41b66
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-03 18:06:24 +0000

    Add files via upload

---
+++ q041 quote.py
@@ -0,0 +1,8 @@
+#Python all tasks q41
+
+quote = input("Please enter a quote from a famous film or book.")
+print(quote.upper())
+print(quote.lower())
+
+
+
---
+++ q042 name.py
@@ -0,0 +1,12 @@
+#Python all tasks q42
+
+name = input("Please enter your name.")
+choice = input("Do you want to print your name upper case or lower case?")
+if choice.lower() == "lower":
+    print(name.lower())
+else:
+    print(name.upper())
+
+
+
+
