0c8ff7a026b8e18677fd7ffe887b63fb7caaa6ba
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-04 10:39:37 +0000

    Add files via upload

---
+++ q043 quote.py
@@ -0,0 +1,6 @@
+#Python all tasks q43
+
+quote = input("Please enter a famous quote.")
+word1 = input("What word in the quote would you like to replace?")
+word2 = input("What wordwould you like to replace it with?")
+print(quote.replace(word1,word2))
