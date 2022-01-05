9b5e5a382dab055558ff5ee7d628e89530dfecc7
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-05 09:59:48 +0000

    Add files via upload

---
+++ q077 arrayReverse.py
@@ -0,0 +1,15 @@
+#python programming tasks q077
+
+##number = []
+##for i in range (0,5):
+##    number.append(int(input("Please enter a number")))
+##    number.sort()
+##number.reverse()
+##print(number)
+
+array = []
+for i in range (0,5):
+    number = int(input("please enter a number"))
+    array.append(number)
+array.reverse()
+print(array)
