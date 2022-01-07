d0f05ab2c530e924da72f8c5d06c4b4792fa0224
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-07 08:06:32 +0000

    Add files via upload

---
+++ q084 2d array.py
@@ -0,0 +1,10 @@
+#python programming tasks q084
+
+grades = [["Alfie Little",24,32,5],\
+          ["Billy Bob Junior 11",22,22,53],\
+          ["Mark Jones",43,54,23],\
+          ["King Plonker",23,12,32]]
+
+print(grades)
+
+
