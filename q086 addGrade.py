ea752c3ce6e88defc158e8c8d8619a20565ae199
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-07 09:39:17 +0000

    Add files via upload

---
+++ q086 addGrade.py
@@ -0,0 +1,20 @@
+#python programming tasks q086
+
+#Use \ to split up a long line of code
+
+grades = [["Alfie Little",24,32,5],\
+          ["Billy Bob Junior ii",22,22,53],\
+          ["Mark Jones",43,54,23],\
+          ["King Plonker",23,12,32]]
+
+print(grades)
+grades[0].append(37)
+grades[1].append(99)
+grades[2].append(32)
+grades[3].append(42)
+print(grades)
+
+
+
+
+
