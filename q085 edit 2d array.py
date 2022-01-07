9d99ba11b6151e4160adfd35005f068ebdb0bcf9
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-07 08:11:31 +0000

    Add files via upload

---
+++ q085 edit 2d array.py
@@ -0,0 +1,14 @@
+#python programming tasks q085
+
+#Use \ to split up a long line of code
+
+grades = [["Alfie Little",24,32,5],\
+          ["Billy Bob Junior 11",22,22,53],\
+          ["Mark Jones",43,54,23],\
+          ["King Plonker",23,12,32]]
+
+print(grades[2])
+grades[2][2]=76
+print(grades[2])
+
+
