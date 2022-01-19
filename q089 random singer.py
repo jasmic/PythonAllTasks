382bbdb7bf7b932653e93c14f2158c0e7f03bcbb
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-01-19 12:29:59 +0000

    Add files via upload

---
+++ q089 random singer.py
@@ -0,0 +1,19 @@
+#python programming tasks q089
+
+#Use \ to split up a long line of code
+
+import random
+array = []
+for i in range(3):
+    singer = input("Give me the name of a singer")
+    array.append(singer)
+    print(array)
+number = random.randint(0,2)
+print(array[number],"has been chosen")
+
+
+
+
+
+
+
