ce693fe4340044428eb44297064017453a51de53
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-02-06 09:42:04 +0000

    Add files via upload

---
+++ q95 writeDoc Films.py
@@ -0,0 +1,13 @@
+#PythonAllTasks q95
+
+
+file = open("test1.txt","w")
+file.write("Blade Runner\n")
+file.write("Interstellar\n")
+file.write("Into the Spiderverse")
+file.close()
+
+
+
+
+
---
+++ q96 writeDoc name.py
@@ -0,0 +1,11 @@
+#PythonAllTasks q96
+
+file = open("test2.txt","w")
+name = input("Please enter your name")
+file.write(name)
+file.close()
+
+
+
+
+
