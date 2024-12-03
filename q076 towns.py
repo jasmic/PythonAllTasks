fd813294b6aff4fe226938c70f3dce28f34aa614
jasmic <50964113+jasmic@users.noreply.github.com>
2024-12-03 13:14:02 +0000

    q076 towns.py
    New version added when tutoring new student.

--- q076 towns.py
+++ q076 towns.py
@@ -5,7 +5,12 @@ print(array[0:1])
 print(array[3:4])
 print(array[0:2])
 
-
-
-
 #print(array[1:3]) #prints Uttoxeter Loxley
+
+# #Python Tasks - this version added 03.12.24
+
+# # #Q78
+# array = ["Barton","Uttoxeter","Loxley"]
+# print(array[0:1],array[2:3])    #prints Barton Loxley in a list
+# print(array[0:1])               #prints Barton in a list
+# print(array[1:2])               #prints Uttoxeter
