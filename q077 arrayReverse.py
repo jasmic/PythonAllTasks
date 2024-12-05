12b8669b25786025d8c46f5d7a8119dac165cb8e
jasmic <50964113+jasmic@users.noreply.github.com>
2024-12-05 18:42:16 +0000

    q077 arrayReverse.py
    New update

--- q077 arrayReverse.py
+++ q077 arrayReverse.py
@@ -13,3 +13,12 @@ for i in range (0,5):
     array.append(number)
 array.reverse()
 print(array)
+
+# Added 05.12.2024
+# Showing how the number input can be added to the array append in a single line
+# #Q77
+array=[]
+for i in range(0,5):
+    array.append(input("Enter a number. "))
+array.reverse()
+print(array)
